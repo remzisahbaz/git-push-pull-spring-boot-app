7cce9a7f487ebfc752aa2ab3b8fce62bc3ded7e9
Remzi ŞAHBAZ <remzi.sahbaz@tim.org.tr>
2024-02-27 12:08:31 +0300

    committed

---
+++ src/main/java/com/remzisahbaz/gitpushpullspringbootapp/GitPushPullSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.remzisahbaz.gitpushpullspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPushPullSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPushPullSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remzisahbaz/gitpushpullspringbootapp/GitPushPullSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remzisahbaz.gitpushpullspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPushPullSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
